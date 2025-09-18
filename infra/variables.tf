513f7863bb2ba9e6abf9851583bc239bac60a25e
Murali Balu <156786504+Murali121290@users.noreply.github.com>
2025-09-18 14:49:20 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,24 @@
-variable "region" { default = "us-east-1" }
-variable "key_name" { description = "SSH key name in AWS" }
-variable "aws_ami" { description = "Ubuntu AMI id" }
-variable "instance_type" { default = "t3.medium" }
-variable "project_tag" { default = "jenkins-sonar-minikube-demo" }
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "key_name" {
+  description = "SSH key name in AWS"
+  default     = "murali26jul2025"   # <-- replace with your actual EC2 key pair name
+}
+
+variable "aws_ami" {
+  description = "Ubuntu 22.04 AMI id"
+  default     = "ami-0a91cd140a1fc148a"   # <-- change to latest Ubuntu AMI in your region
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.medium"
+}
+
+variable "project_tag" {
+  description = "Project tag to label AWS resources"
+  default     = "jenkins-sonar-minikube-demo"
+}
